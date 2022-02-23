1e14fed5c3bca3ff91d70bbff0ec02d3efb842e9
Luiz Eduardo Gagno Amancio <luizgagnoamancio@gmail.com>
2022-02-23 14:42:03 -0300

    pokemon_game_start

---
+++ Pokemon/Player/Player.gd
@@ -0,0 +1,53 @@
+extends KinematicBody2D
+
+
+export var walk_speed = 4.0
+const TILE_SIZE = 16
+
+onready var anim_tree = $AnimationTree
+onready var anim_state = anim_tree.get("parameters/playback")
+
+var initial_position = Vector2(0, 0)
+var input_direction = Vector2(0, 0)
+var is_moving = false
+var percent_moved_to_next_tile = 0.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	initial_position = position
+	
+
+func _physics_process(delta):
+	if is_moving == false:
+		process_player_input()
+	elif input_direction != Vector2.ZERO:
+		anim_state.travel("Walk")
+		move(delta)
+	else:
+		anim_state.travel("Idle")
+		is_moving = false
+
+func process_player_input():
+	if input_direction.y == 0:
+		input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	if input_direction.x == 0:
+		input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	
+	if input_direction != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_direction)
+		anim_tree.set("parameters/Walk/blend_position", input_direction)
+		initial_position = position
+		is_moving = true
+	else:
+		anim_state.travel("Idle")
+		
+		
+#delta is the amount of time passed since the last frame
+func move(delta):
+	percent_moved_to_next_tile += walk_speed * delta
+	if percent_moved_to_next_tile >= 1.0:
+		position = initial_position + (input_direction * TILE_SIZE)
+		percent_moved_to_next_tile = 0
+		is_moving = false
+	else:
+		position = initial_position + (TILE_SIZE * input_direction * percent_moved_to_next_tile)
